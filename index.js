4f32cca04ecd66c97c942cd1dfbc13b0bd08c917
Alpha <alpha.likembe@student.moringaschool.com>
2023-07-20 00:32:12 +0300

    Pending Clearance

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// code your solution here
+
+function saturdayFun(name, activity = "roller-skate!"){
+    console.log(`${name} This saturday, I want to ${activity}`);
+}
+saturdayFun("");
+saturdayFun("", "bathe my dog!");
+
+function mondayWork(name, activity = "go to the office."){
+    console.log(`${name} "This Monday, I will ${activity}`);
+
+}
+mondayWork("");
+mondayWork("", 'work from home.')
+
+function wrapAdjective(symbol){
+    return function(adjective){
+        return `You are ${symbol} ${adjective} ${symbol}!`;
+    };
+}
+    let result = wrapAdjective("*");
+    let emphatic = result("a hard worker");
+    console.log(emphatic);
+
+    let result2 = wrapAdjective("||");
+    let emphatic2 = result2("a dedicated programmer");
+    console.log(emphatic2);
+
